5aa0cabdd8cbf2410a938a85fabcfae9f19ee994
Saood Imran <saood.imran@uoit.net>
2019-11-28 12:10:56 -0500

    Update loadpage.js
    Adding in new function to print if there is no data returned.

--- loadpage.js
+++ loadpage.js
@@ -16,20 +16,32 @@ function loadpage(aURL){
 function tableformat(jsonData){
 	console.log(jsonData);
 	var parsedJson = JSON.parse(jsonData);
-	console.log(parsedJson);
-	var columns = [];
-		$.each(parsedJson[0], function(key,value){
-		var records = {};
-		records.title = key;
-		records.data = key;
-		columns.push(records);
-		console.log("key: "+ key + " value: "+ value);
-	});
-	console.log(columns);
-	$(document).ready(function(){
-		table = $('#outtable').DataTable({
-			data: parsedJson,
-			"columns": columns
+	console.log(parsedJson.length)
+	if(parsedJson.length == 1)
+	{
+		var node = document.createElement("h5");
+		var text = document.createTextNode("No data");
+		node.appendChild(text);
+		document.getElementById("nodata").appendChild(node)
+
+	}
+	else
+	{
+		console.log(parsedJson);
+		var columns = [];
+			$.each(parsedJson[0], function(key,value){
+			var records = {};
+			records.title = key;
+			records.data = key;
+			columns.push(records);
+			console.log("key: "+ key + " value: "+ value);
 		});
-	});
+		console.log(columns);
+		$(document).ready(function(){
+			table = $('#outtable').DataTable({
+				data: parsedJson,
+				"columns": columns
+			});
+		});
+	}
 }
